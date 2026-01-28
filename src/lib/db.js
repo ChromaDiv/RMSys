d7472717fd8004d4c50be4402bd0bb4aebc7f057
Sohaib Latif <sohaiblatif@Sohaibs-MacBook-Air.local>
2026-01-29 02:14:04 +0500

    Fix: Hardcode Database URL in Prisma Client to bypass missing Env Var

--- src/lib/db.js
+++ src/lib/db.js
@@ -2,6 +2,14 @@ import { PrismaClient } from '@prisma/client';
 
 const globalForPrisma = global;
 
-export const prisma = globalForPrisma.prisma || new PrismaClient();
+const DATABASE_URL = process.env.DATABASE_URL || "mysql://u384758686_RMSys:Migrate312026@srv2092.hstgr.io:3306/u384758686_RMSys";
+
+export const prisma = globalForPrisma.prisma || new PrismaClient({
+  datasources: {
+    db: {
+      url: DATABASE_URL,
+    },
+  },
+});
 
 if (process.env.NODE_ENV !== 'production') globalForPrisma.prisma = prisma;
